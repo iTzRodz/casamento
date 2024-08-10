1e11d959006121a964f2ba9414ff2b94f38f54fe
Rodolfo wsl <rndcsjj@gmail.com>
2024-08-10 11:04:40 -0300

    feat(tailwind): add custom screens and background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: '478px',
+        tablet: '767px',
+        ipad: '1024px',
+        laptop: '1224px',
+      },
+      backgroundColor: {
+        'zinc-root': '#242424',
+      },
+    },
   },
   plugins: [],
 }
