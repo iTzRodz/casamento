31d1eb6ef1a7b3f145fd21fa3c97a8ebbe5211d2
Rodolfo wsl <rndcsjj@gmail.com>
2025-03-11 20:35:48 -0300

    feat: icons

--- src/components/icons/ArrowRightIcon.tsx
+++ src/components/icons/ArrowRightIcon.tsx
@@ -8,7 +8,7 @@ export const ArrowRightIcon = ({ className }: ArrowProps) => (
     height="24"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="#000000"
+    stroke="currentColor"
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
