5496bfa96fca66937ec4d60d331f06d96c8e8955
Rodolfo <rndcsjj@gmail.com>
2025-06-05 18:57:33 -0300

    feat: improve layout and responsiveness of Button, ListItens, ListItensSelected, and Cart components

--- src/components/Modal/Cart.tsx
+++ src/components/Modal/Cart.tsx
@@ -30,47 +30,51 @@ export function Cart({ handleCloseModal }: ConfirmationPaymentProps) {
   }
 
   return (
-    <section className="fixed  left-0 top-0 z-50 flex h-screen w-full items-center justify-center overflow-auto px-2">
-      <form className="m-auto w-[768px] rounded-xl border-[#515E6C8C] bg-[#1D212A] shadow-md">
-        {!showOrderSummary ? (
-          <div className="flex w-full flex-col justify-start rounded-lg border border-transparent px-8 py-4 shadow">
-            <span
-              className="flex cursor-pointer justify-end hover:opacity-80"
-              onClick={handleCloseModal}
-            >
-              <CloseIcon />
-            </span>
+    <section className="fixed inset-0 z-10 overflow-y-auto">
+      <div className="flex min-h-full items-center justify-center p-4 text-center">
+        <div className="w-full max-w-[530px] overflow-hidden rounded-xl border border-[#515E6C]/55 bg-[#1D212A] p-4 text-left align-middle shadow-xl transition-all mobile:max-w-3xl">
+          <form className="flex w-full flex-col gap-2.5">
+            {!showOrderSummary ? (
+              <div className="flex w-full flex-col justify-start rounded-lg border border-transparent py-4 shadow mobile:px-8">
+                <span
+                  className="flex cursor-pointer justify-end hover:opacity-80"
+                  onClick={handleCloseModal}
+                >
+                  <CloseIcon />
+                </span>
 
-            <h1 className="mt-4 text-4xl font-semibold text-white mobile:text-5xl">
-              Lista de presentes
-            </h1>
-            <ListItensSelected />
-            <div className="mt-10 flex flex-col items-center justify-center gap-5 mobile:flex-row">
-              <Button
-                variant="primary"
-                className="w-full"
-                onClick={handleCloseModal}
-                type="button"
-              >
-                Adicionar mais itens
-              </Button>
-              <Button
-                variant="primary"
-                className={`w-full bg-white text-black ${showOrderSummary && "hidden"}`}
-                arrowIcon
-                type="button"
-                onClick={() => setShowOrderSummary(true)}
-              >
-                Continuar compra
-              </Button>
-            </div>
-          </div>
-        ) : (
-          <OrderSummary
-            handleCloseModalOrderSummary={handleCloseModalOrderSummary}
-          />
-        )}
-      </form>
+                <h1 className="mt-4 text-3xl font-semibold text-white mobile:text-5xl">
+                  Lista de presentes
+                </h1>
+                <ListItensSelected />
+                <div className="mt-10 flex flex-col items-center justify-center gap-5 py-4 mobile:flex-row">
+                  <Button
+                    variant="primary"
+                    className="w-full"
+                    onClick={handleCloseModal}
+                    type="button"
+                  >
+                    Adicionar mais itens
+                  </Button>
+                  <Button
+                    variant="primary"
+                    className={`w-full bg-white text-black ${showOrderSummary && "hidden"}`}
+                    arrowIcon
+                    type="button"
+                    onClick={() => setShowOrderSummary(true)}
+                  >
+                    Continuar compra
+                  </Button>
+                </div>
+              </div>
+            ) : (
+              <OrderSummary
+                handleCloseModalOrderSummary={handleCloseModalOrderSummary}
+              />
+            )}
+          </form>
+        </div>
+      </div>
     </section>
   );
 }
