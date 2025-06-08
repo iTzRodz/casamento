7908bfd6b07f048ece4711ef3c0b25e7903ba409
Rodolfo <rndcsjj@gmail.com>
2025-06-08 15:23:31 -0300

    feat: remove unused API routes, controllers, and database migrations

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import serverless from "serverless-http";
-import app from "../src/api/server";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const handler = serverless(app as any);
-
-export default handler;
--- prisma/migrations/20250215223028_remove_unique_person_id/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `Companion` DROP FOREIGN KEY `Companion_personId_fkey`;
-
--- DropIndex
-DROP INDEX `Companion_personId_key` ON `Companion`;
-
--- AddForeignKey
-ALTER TABLE `Companion` ADD CONSTRAINT `Companion_personId_fkey` FOREIGN KEY (`personId`) REFERENCES `Person`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
